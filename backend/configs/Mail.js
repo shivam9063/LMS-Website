214719d4124e1ee6919dc5c5bdc02d9f9bfd0547
Shivam Pathak <144466361+shivam9063@users.noreply.github.com>
2026-06-15 00:55:52 +0530

    Update Mail.js

--- backend/configs/Mail.js
+++ backend/configs/Mail.js
@@ -5,29 +5,15 @@ dotenv.config()
 const senderEmail = process.env.EMAIL?.trim()
 const senderPassword = process.env.EMAIL_PASS?.replace(/\s+/g, "")?.trim()
 
-// const transporter = nodemailer.createTransport({
-//     host: "smtp.gmail.com",
-//     port: 465,
-//     secure: true,
-//     auth: {
-//         user: senderEmail,
-//         pass: senderPassword,
-//     },
-// })
-
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.EMAIL,
-    pass: process.env.EMAIL_PASS,
-  },
-  tls: {
-    rejectUnauthorized: false,
-  },
-});
-
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+        user: senderEmail,
+        pass: senderPassword,
+    },
+})
 
 const sendMail = async (to, otp) => {
     try {
